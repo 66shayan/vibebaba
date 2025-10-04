74fec1c595c1bd09b16bc22c6db3ffe07bf2e23c
Shayan Tawabi <66shayan@gmail.com>
2025-10-04 16:58:37 +0330

    Delete functions/[[path]].ts

--- functions/[[path]].ts
+++
@@ -1,5 +0,0 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-// @ts-ignore - build output
-import * as build from "../build/server";
-
-export const onRequest = createPagesFunctionHandler({ build });
